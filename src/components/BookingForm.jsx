7f4438933b0371a17b7319404631f935a871c19d
N16H7H4WK-3R <aryan014kumar@gmail.com>
2024-12-02 00:26:37 +0530

    minor fix

--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { API_BASE_URL, AUTH_TOKEN } from "./Api";
+import { API_BASE_URL, AUTH_TOKEN, fetchAPI } from "./Api";
 
 
 
